8845252f6623675b0674d3468622e6dc6a5ebb0c
jul-star <jstar@rambler.ru>
2020-03-30 09:25:53 +0300

    Added 03_UniqueString

---
+++ 01_white/03_UniqueString/UniqueString/UniqueString.cpp
@@ -0,0 +1,5 @@
+#include "UniqueString.h"
+
+UniqueString::UniqueString()
+{
+}
---
+++ 01_white/03_UniqueString/UniqueString/UniqueString.h
@@ -0,0 +1,12 @@
+#ifndef UNIQUESTRING_H
+#define UNIQUESTRING_H
+
+#include "UniqueString_global.h"
+
+class UNIQUESTRING_EXPORT UniqueString
+{
+public:
+    UniqueString();
+};
+
+#endif // UNIQUESTRING_H
