4da62cedf8ad9081ec5b08c03487a05149170fa8
jul-star <jstar@rambler.ru>
2020-05-11 10:42:05 +0300

    Working on yl_w1_e04.h

--- 02_yellow/week_01/Source/yl_w1_e04.h
+++ 02_yellow/week_01/Source/yl_w1_e04.h
@@ -11,10 +11,10 @@ using namespace ::std;
 
 namespace yellow::w1::e04 {
 enum class TaskStatus {
-  NEW,          // новая
-  IN_PROGRESS,  // в разработке
-  TESTING,      // на тестировании
-  DONE          // завершена
+    NEW,          // новая
+    IN_PROGRESS,  // в разработке
+    TESTING,      // на тестировании
+    DONE          // завершена
 };
 
 using TasksInfo = map<TaskStatus, int>;
@@ -22,25 +22,148 @@ using TasksInfo = map<TaskStatus, int>;
 class TeamTasks {
     map<string, TasksInfo> vPersonTask;
 public:
-  // Получить статистику по статусам задач конкретного разработчика
-  const TasksInfo& GetPersonTasksInfo(const string& person) const
-  {
-      return vPersonTask.at(person);
-  }
+    // Получить статистику по статусам задач конкретного разработчика
+    const TasksInfo& GetPersonTasksInfo(const string& person) const
+    {
+        return vPersonTask.at(person);
+    }
 
-  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-  void AddNewTask(const string& person)
-  {
-      ++vPersonTask[person][TaskStatus::NEW];
-  }
+    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+    void AddNewTask(const string& person)
+    {
+        ++vPersonTask[person][TaskStatus::NEW];
+    }
 
-  // Обновить статусы по данному количеству задач конкретного разработчика,
-  // подробности см. ниже
-  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-      const string& person, int task_count)
-  {
-      return {};
-  }
+    int GetRefreshNumber(int number, int &task_count)
+    {
+        int toRefresh{0};
+        if(number < task_count)
+        {
+            toRefresh = number;
+            task_count -= number;
+        }else{
+            toRefresh = task_count;
+            task_count = 0;
+        }
+        return toRefresh;
+    }
+
+    void RefreshTaskStatus(TasksInfo const & ti, TaskStatus status, TasksInfo & result, int & task_count)
+    {
+        if(ti.find(status) != ti.end())
+        {
+            result[status] = GetRefreshNumber(ti.at(status), task_count);
+        }
+    }
+
+    void BuildWithNewStatus(TasksInfo const & delist, TaskStatus cur,
+                            TasksInfo & new_status, TaskStatus next )
+    {
+        if (delist.find(cur) != delist.end())
+        {
+            new_status[next] = delist.at(cur);
+        }
+    }
+
+    tuple<TasksInfo, TasksInfo> GetTasksToDelistAndWithNewStatus(TasksInfo & ti, int task_count)
+    {
+        if (task_count == 0)
+        {
+            return {};
+        }
+        TasksInfo ToDelist;
+        TasksInfo WithNewStatus;
+
+        RefreshTaskStatus(ti, TaskStatus::NEW, ToDelist, task_count);
+        BuildWithNewStatus(ToDelist, TaskStatus::NEW, WithNewStatus, TaskStatus::IN_PROGRESS);
+        if(task_count == 0){ return tie(ToDelist,WithNewStatus); }
+
+        RefreshTaskStatus(ti, TaskStatus::IN_PROGRESS, ToDelist, task_count);
+        BuildWithNewStatus(ToDelist, TaskStatus::IN_PROGRESS, WithNewStatus, TaskStatus::TESTING);
+        if(task_count == 0){ return tie(ToDelist,WithNewStatus); }
+
+        RefreshTaskStatus(ti, TaskStatus::TESTING, ToDelist, task_count);
+        BuildWithNewStatus(ToDelist, TaskStatus::TESTING, WithNewStatus, TaskStatus::DONE);
+        if(task_count == 0){ return tie(ToDelist,WithNewStatus); }
+
+        RefreshTaskStatus(ti, TaskStatus::DONE, ToDelist, task_count);
+        return tie(ToDelist,WithNewStatus);
+    }
+
+    int SubtractOldTask(TasksInfo & ti, TasksInfo const & delist, TaskStatus status)
+    {
+        int old{0};
+        if(delist.find(status) != delist.end())
+        {
+            if(ti.find(status) == ti.end())
+            {
+                ti[status]=0;
+            }
+            old = ti.at(status) - delist.at(status);
+            ti[status] = old;
+        }
+        return old;
+    }
+    TasksInfo GetOldTask(TasksInfo & ti, TasksInfo const & delist)
+    {
+        TasksInfo old;
+        int vOld{0};
+        vOld = SubtractOldTask(ti, delist, TaskStatus::NEW);
+        if (vOld != 0) {old[TaskStatus::NEW] = vOld; vOld = 0;}
+
+        vOld = SubtractOldTask(ti, delist, TaskStatus::IN_PROGRESS);
+        if (vOld != 0) {old[TaskStatus::IN_PROGRESS] = vOld; vOld = 0;}
+
+        vOld = SubtractOldTask(ti, delist, TaskStatus::TESTING);
+        if (vOld != 0) {old[TaskStatus::TESTING] = vOld; vOld = 0;}
+
+        vOld = SubtractOldTask(ti, delist, TaskStatus::DONE);
+        if (vOld != 0) {old[TaskStatus::DONE] = vOld; }
+        return old;
+    }
+
+    void Add(TasksInfo & ti, TasksInfo const & nw, TaskStatus status)
+    {
+        if(nw.find(status)!= nw.end())
+        {
+            if(nw.at(status) != 0)
+            {
+                ti[status] += nw.at(status);
+            }
+        }
+    }
+
+    void AddTaskWithNewStatus(TasksInfo & ti, TasksInfo const & nw)
+    {
+        Add(ti, nw, TaskStatus::NEW);
+        Add(ti, nw, TaskStatus::IN_PROGRESS);
+        Add(ti, nw, TaskStatus::TESTING);
+        Add(ti, nw, TaskStatus::DONE);
+    }
+
+    // Обновить статусы по данному количеству задач конкретного разработчика,
+    // подробности см. ниже
+    tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+            const string& person, int task_count)
+    {
+        if(task_count == 0 )
+        {
+            return {};
+        }
+        TasksInfo old;
+        TasksInfo nw;
+        TasksInfo delist;
+        int tasks = task_count;
+        if (vPersonTask.find(person) != vPersonTask.end())
+        {
+            TasksInfo ti = vPersonTask.find(person)->second;
+            tie(delist, nw) = GetTasksToDelistAndWithNewStatus(ti, tasks);
+            old = GetOldTask(ti, delist);
+            AddTaskWithNewStatus(ti, nw);
+        }
+
+        return tie<TasksInfo, TasksInfo>(nw, old);
+    }
 };
 
 }
--- 02_yellow/week_01/UTest/tst_yl_w1_e04.h
+++ 02_yellow/week_01/UTest/tst_yl_w1_e04.h
@@ -4,64 +4,156 @@
 #include <gmock/gmock-matchers.h>
 #include <yl_w1_e04.h>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace ::std;
 using namespace ::yellow::w1::e04;
 using namespace ::testing;
 
 namespace yellow::w1::ts  {
-class yl_w1_e03_t:public ::testing::Test{
+class yl_w1_e04_t:public ::testing::Test{
 
 };
 
-void PrintTasksInfo(TasksInfo tasks_info) {
-  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+string PrintTasksInfo(TasksInfo tasks_info) {
+    stringstream ss;
+    ss << tasks_info[TaskStatus::NEW] << " new tasks" <<
+                                         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+                                         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+                                         ", " << tasks_info[TaskStatus::DONE] << " tasks are done";
+    return ss.str();
 }
 
-TEST_F(yl_w1_e03_t, TaskInfo_00)
+TEST_F(yl_w1_e04_t, TaskInfo_00)
+{
+    TaskStatus status{TaskStatus::DONE};
+    TaskStatus next_status = static_cast<TaskStatus>(static_cast<int>(status)+1);
+
+    EXPECT_NE(TaskStatus::NEW, next_status) << "next: " << static_cast<int>(next_status);
+    EXPECT_NE(TaskStatus::IN_PROGRESS, next_status) << "next: " << static_cast<int>(next_status);
+    EXPECT_NE(TaskStatus::TESTING, next_status) << "next: " << static_cast<int>(next_status);
+    EXPECT_NE(TaskStatus::DONE, next_status) << "next: " << static_cast<int>(next_status);
+}
+
+TEST_F(yl_w1_e04_t, TaskInfo_01)
+{
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia");
+    string expIlia{"1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+    string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+    EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+}
+
+TEST_F(yl_w1_e04_t, TaskInfo_02)
+{
+    TeamTasks tasks;
+    string person{"Ilia"};
+    tasks.AddNewTask(person);
+    {
+        string expIlia{"1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo(person));
+        EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+    }
+
+    TasksInfo updated_tasks, untouched_tasks;
+    tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks(person, 1);
+
+    {
+        string expIlia{"0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo(person));
+        EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+    }
+}
+
+TEST_F(yl_w1_e04_t, TaskInfo_03)
+{
+    TeamTasks tasks;
+    string person{"Ilia"};
+    tasks.AddNewTask(person);
+    tasks.AddNewTask(person);
+
+    {
+        string expIlia{"2 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo(person));
+        EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+    }
+
+    TasksInfo updated_tasks, untouched_tasks;
+    tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks(person, 1);
+
+    {
+        string expIlia{"1 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo(person));
+        EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+    }
+}
+
+
+
+TEST_F(yl_w1_e04_t, TaskInfo_11)
 {
     TeamTasks tasks;
     tasks.AddNewTask("Ilia");
     for (int i = 0; i < 3; ++i) {
-      tasks.AddNewTask("Ivan");
+        tasks.AddNewTask("Ivan");
     }
-    cout << "Ilia's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-    cout << "Ivan's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 
-    TasksInfo updated_tasks, untouched_tasks;
+    {
+        //    cout << "Ilia's tasks: ";
+        string expIlia{"1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIlia = PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+        EXPECT_EQ(expIlia, actIlia) << "actIlia: act: " << actIlia;
+    }
 
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
+    {
+        //    cout << "Ivan's tasks: ";
+        string expIvan{"3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        string actIvan = PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+        EXPECT_EQ(expIvan, actIvan) << "Ivan's tasks: act: " << actIvan;
+    }
 
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
+    {
+        TasksInfo updated_tasks, untouched_tasks;
+        tie(updated_tasks, untouched_tasks) =
+                tasks.PerformPersonTasks("Ivan", 2);
+        //    cout << "Updated Ivan's tasks: ";
+        string actIvanM =PrintTasksInfo(updated_tasks);
+        string expIvanM{"0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        EXPECT_EQ(expIvanM, actIvanM) << "Updated Ivan's tasks: act: " << actIvanM;
 
+        //        cout << "Untouched Ivan's tasks: ";
+        string actIvanU = PrintTasksInfo(untouched_tasks);
+        string expIvanU{"1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        EXPECT_EQ(expIvanU, actIvanU) << "Untouched Ivan's tasks: act: " << actIvanU;
+    }
+
+    {
+        TasksInfo updated_tasks, untouched_tasks;
+        tie(updated_tasks, untouched_tasks) =
+                tasks.PerformPersonTasks("Ivan", 2);
+        cout << "Updated Ivan's tasks: ";
+        string actIvanM = PrintTasksInfo(updated_tasks);
+        string expIvanM{"0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done"};
+        EXPECT_EQ(expIvanM, actIvanM) << "Updated Ivan's tasks: act: " << actIvanM;
+
+        cout << "Untouched Ivan's tasks: ";
+        string actIvanU = PrintTasksInfo(untouched_tasks);
+        string expIvanU{"0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done"};
+        EXPECT_EQ(expIvanU, actIvanU) << "Untouched Ivan's tasks: act: " << actIvanU;
+    }
     //expected:
-//    Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-//    Ivan's tasks: 3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-//    Updated Ivan's tasks: 0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done
-//    Untouched Ivan's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-//    Updated Ivan's tasks: 0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done
-//    Untouched Ivan's tasks: 0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done
+    //    Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
+    //    Ivan's tasks: 3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
+    //    Updated Ivan's tasks: 0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done
+    //    Untouched Ivan's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
+    //    Updated Ivan's tasks: 0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done
+    //    Untouched Ivan's tasks: 0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done
 }
 
-TEST_F(yl_w1_e03_t, TaskInfo_01)
-{
 
-}
 
 }
 
