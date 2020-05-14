66511c0c66c0fddca547bbfd43ba2339b61c1664
jul-star <jstar@rambler.ru>
2020-05-14 10:58:45 +0300

    Done with yl_w1_e05.

--- 02_yellow/week_01/Source/yl_w1_e05.h
+++ 02_yellow/week_01/Source/yl_w1_e05.h
@@ -1,11 +1,52 @@
 #pragma once
 #include <vector>
 #include <map>
-#include <set>
+#include <utility>
 
 using namespace ::std;
 
 namespace yellow::w1::e05 {
+template <typename T>
+vector<T> Sqr(vector<T> & v);
+template <typename Key, typename Value>
+map<Key, Value>  Sqr(map<Key, Value> & m);
+template <typename First, typename Second>
+pair<First, Second> Sqr(pair<First, Second> &p);
+template <typename First, typename Second>
+pair<First, Second> operator*(pair<First, Second> &l, pair<First, Second> &r);
 
+template <typename T>
+vector<T> Sqr(vector<T> & v)
+{
+    for(auto & t: v)
+    {
+        t = t*t;
+    }
+    return v;
+}
+
+template <typename Key, typename Value>
+map<Key, Value>  Sqr(map<Key, Value> & m)
+{
+    for(auto &[key, value]: m)
+    {
+        m[key] = value*value;
+    }
+    return m;
+}
+
+template <typename First, typename Second>
+pair<First, Second> Sqr(pair<First, Second> &p)
+{
+    p.first = p.first*p.first;
+    p.second = p.second*p.second;
+    return p;
+}
+
+template <typename First, typename Second>
+pair<First, Second> operator*(pair<First, Second> &l, pair<First, Second> &r)
+{
+    return make_pair(l.first*r.first, l.second*r.second);
+}
 
 }
--- 02_yellow/week_01/UTest/tst_yl_w1_e05.h
+++ 02_yellow/week_01/UTest/tst_yl_w1_e05.h
@@ -17,12 +17,61 @@ class yl_w1_e05_t:public ::testing::Test{
 };
 
 
-TEST_F(yl_w1_e05_t, TaskInfo_00)
+TEST_F(yl_w1_e05_t, Vec_01)
 {
-
+    vector<int> v = {1, 2, 3};
+    stringstream ss;
+    ss << "vector:";
+    for (int x : Sqr(v)) {
+        ss << ' ' << x;
+    }
+    string expected {"vector: 1 4 9"};
+    EXPECT_EQ(expected, ss.str()) << ss.str();
+    //
 }
 
+TEST_F(yl_w1_e05_t, map_simple)
+{
+    map<int, int> map_simple = {
+        {4, 2},
+        {7, 4}
+    };
+    stringstream ss;
+    ss << "map_simple:" << endl;
+    map<int, int> actual = Sqr(map_simple);
+    for (const auto& x : actual) {
+        ss << x.first << ' ' << x.second << endl;
+    }
+    string expected {"map_simple:\n4 4\n7 16\n"};
+    EXPECT_EQ(expected, ss.str()) << ss.str();
+}
 
+TEST_F(yl_w1_e05_t, pair_simple)
+{
+    pair<int, int> pair_simple = {4, 2};
+    stringstream ss;
+    ss << "pair_simple:" << endl;
+    pair<int, int> actual = Sqr(pair_simple);
+        ss << actual.first << ' ' << actual.second;
+    string expected {"pair_simple:\n16 4"};
+    EXPECT_EQ(expected, ss.str()) << ss.str();
+}
+
+TEST_F(yl_w1_e05_t, map_of_pairs)
+{
+    map<int, pair<int, int>> map_of_pairs = {
+        {4, {2, 2}},
+        {7, {4, 3}}
+    };
+    stringstream ss;
+    ss << "map of pairs:" << endl;
+    map<int, pair<int, int>> actual = Sqr(map_of_pairs);
+    for (const auto& x : actual) {
+        ss << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+    }
+    string expected {"map of pairs:\n4 4 4\n7 16 9\n"};
+    EXPECT_EQ(expected, ss.str()) << ss.str();
+}
 
 }
 
